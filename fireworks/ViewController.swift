2db4f7c433eb527b5e8021b02c3f83573d9148b7
Eric <erictsai1010@gmail.com>
2017-08-22 22:43:26 -0700

    Initial Commit

---
+++ fireworks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fireworks
+//
+//  Created by Eric on 2017/08/22.
+//  Copyright © 2017 Eric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
